69ab220cc1a2ea45f9b74679bd68760b181c04c8
roostie-kam <kam22008@byui.edu>
2023-03-25 20:28:50 -0600

    Breathing assignment edit

--- week_6/Developer/Program.cs
+++ week_6/Developer/Program.cs
@@ -1,74 +1,45 @@
 // File: Program.cs //
-// Date: 2/27/2023 // 
+// Date: 3/11/2023 // 
 // Author: Scott Kamerath // 
 // Purpose: To learn how to write an Encapsulation code (Scripture Memorizer) in CS#. //
 // Scripture: Ecclesiastes 3:1-8. //
 
 using System;
-using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 public class Program
 {
-    public static string ScriptureTest { get; private set; }
+    private object dateojb;
+    private object asdasdasd;
+
+    public object Date { get; private set; }
+    public object Test { get; private set; }
 
     static void Main(string[] args)
     {
-    string.Join;
-    string prompt;
-    string response;
-    DateTime dateojb = DateTime.Now; 
-    string date;
-
-    static void Display()
+        DateTime dateojb = DateTime.Now; 
+    }
+    public void Display()
     {
         Console.WriteLine("book");
         Console.WriteLine("Chapter");
         Console.WriteLine("verse");
         Console.WriteLine("save");
         Console.WriteLine("quit");
+        Thread.Sleep(8000);
     }
-
-// Create a list of verses.  
-        var versus = new List<string>();  
-        verse.Add("1");  
-        verse.Add("2");  
-        verse.Add("3");  
-        verse.Add("4");  
-        verse.Add("5");  
-        verse.Add("6");  
-        verse.Add("7");  
-        verse.Add("8");  
-
-        // Iterate list element using foreach loop. 
-        foreach (var country in countries)  
-        {  
-            Console.WriteLine(country);  
-        }
-
-    static void Write(DateTime dateojb, string date)
+    public void Write()
     {
-        date = dateojb.ToShortDateString();
-    }
-    
-    {
+        // DateTime = dateojb.ToShortDateString;
+        Console.WriteLine("1 2 3 4 5 6 7 8");
+        Console.ReadLine();        
+        Console.Clear();
+        Console.WriteLine("1 2 3 4 5 6 7 8");
         TextRemover remover = new TextRemover();
-        string originalText = ScriptureTest; 1,2,3,34343, asdasdasd;
+        string originalText = "Scripture Test 1, 2, 3, 34343, asdasdasd";
         remover.SetOriginalText(originalText);
         string modifiedText = remover.GetModifiedText();
         Console.WriteLine("Original text:" + originalText);
         Console.WriteLine("Modified text:" + modifiedText);
-    }
-    }
-
-    private class TextRemover
-    {
-        public TextRemover()
-        {
-        }
-
-        internal void SetOriginalText(string originalText)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
--- week_6/Developer/Reference.cs
+++ week_6/Developer/Reference.cs
@@ -1,8 +1,8 @@
 // File: Reference.cs //
-// Date: 2/27/2023 // 
+// Date: 3/11/2023 // 
 // Author: Scott Kamerath // 
 // Purpose: To learn how to write an Encapsulation code (Scripture Memorizer) in CS#. //
-// Scripture: Ecclesiastes 3:1-8. //
+// Scripture: Ecclesiastes 3:1-4. //
 
 using System;
 
@@ -11,6 +11,9 @@ public class Reference
     private string _book = "Ecclesiastes";
     private string _chapter = "Chapter 3";
     private string _verse = "1-8";
+    private string originalText;
+    private string modifiedText;
+
     public void Display() 
     {
         // display function
